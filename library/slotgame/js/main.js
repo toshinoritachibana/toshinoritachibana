6a7f3f214a81e167b8f600ed820ca874c4f5b1d9
toshinoritachibana <89832837+toshinoritachibana@users.noreply.github.com>
2021-09-02 00:09:32 +0900

    Add files via upload

---
+++ library/slotgame/js/main.js
@@ -0,0 +1,78 @@
+'use strict';
+
+{
+  class Panel {
+    constructor() {
+      const section = document.createElement('section');
+      section.classList.add('panel');
+
+      this.img = document.createElement('img');
+      this.img.src = this.getRandomImage();
+
+      this.timeoutId = undefined;
+
+      this.stop = document.createElement('div');
+      this.stop.textContent = 'STOP';
+      this.stop.classList.add('stop');
+      this.stop.addEventListener('click' , () => {
+        clearTimeout(this.timeoutId);
+
+        panelsLeft--;
+        if (panelsLeft === 0) {
+          checkResult();
+        }
+
+      });
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+
+      const main = document.querySelector('main');
+      main.appendChild(section);
+    }
+
+    getRandomImage() {
+      const images = [
+        'img/seven.png',
+        'img/bell.png',
+        'img/cherry.png',
+      ];
+      return images[Math.floor(Math.random() * images.length)]; 
+    }
+
+    spin() {
+      this.img.src = this.getRandomImage();
+      this.timeoutId = setTimeout(() => {
+        this.spin();
+      }, 50);  
+    }
+  }
+
+  function choeckResult() {
+    if (panels[0].isUnmatched(panels[1], panels[2])) {
+      panels[0].unmatch();
+    }
+    if (panels[1].isUnmatched(panels[0], panels[2])) {
+      panels[1].unmatch();
+    }
+    if (panels[2].isUnmatched(panels[0], panels[1])) {
+      panels[2].unmatch();
+    }
+  }
+
+  const panels = [
+    new Panel(),
+    new Panel(), 
+    new Panel(),
+  ];
+
+  let panelsLeft = 3;
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click' , () => {
+    panels.forEach(panel => {
+      panel.spin(); 
+    });
+
+  });
+}
